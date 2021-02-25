ae167deb7db490c9e4c8dc0b341070ad879eb9a0
Andrés López <andres.lopez@wolox.co>
2021-02-25 16:31:26 -0500

    fix jsonPlaceHolderApi envs config

--- config/index.js
+++ config/index.js
@@ -45,9 +45,6 @@ const config = {
       packageVersion: process.env.PACKAGE_VERSION || 'X-Package-Version',
       nodeVersion: process.env.NODE_VERSION || 'X-Node-Version'
     }
-  },
-  jsonPlaceHolderApi: {
-    baseUrl: process.env.JSON_PLACE_HOLDER_API_BASE_URL
   }
 };
 
