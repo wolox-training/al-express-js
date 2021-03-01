88c67ae951136e4fefce187df4eeb04e767b813f
Andrés López <andres.lopez@wolox.co>
2021-03-01 07:55:07 -0500

    fix signUp test

--- test/app/services/users.spec.js
+++ test/app/services/users.spec.js
@@ -16,11 +16,11 @@ describe('Users Service', () => {
 
     expect(userService.signUp).toHaveBeenCalledWith(userMock);
     expect(response).toBeDefined();
-    expect(response.dataValues).toBeDefined();
-    expect(response.dataValues.id).toBeTruthy();
-    expect(response.dataValues.createdAt).toBeTruthy();
-    expect(response.dataValues.updatedAt).toBeTruthy();
-    expect(response.dataValues).toMatchObject(omit(userMock, ['password']));
+    expect(response).toBeDefined();
+    expect(response.id).toBeTruthy();
+    expect(response.createdAt).toBeTruthy();
+    expect(response.updatedAt).toBeTruthy();
+    expect(response).toMatchObject(omit(userMock, ['password']));
   });
 
   it('should fail when is signing up an user with an existing email', async () => {
